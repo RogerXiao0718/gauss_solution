56d8e55b00e356a01983aab122fccc6fe85e3d1f
RogerXiao <deo2468977@gmail.com>
2020-03-10 16:01:04 +0800

    create ask function

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
 readline = require("readline");
 
 const ask = function() {
-    
+    return new Promise((resolve, reject) => {
+        rl = readline.createInterface({
+            input: process.input,
+            output: process.output
+        })
+        rl.question("Enter a list of number(space seperated): ", function(answer){
+            resolve(answer);
+        })
+    })
 }

02e940914fbc4b6898763bcde09226fad9663e16
Roger Xiao <deo2468977@gmail.com>
2020-03-10 21:46:31 +0800

    complete!!

--- ask.js
+++ ask.js
@@ -1,15 +1,16 @@
-readline = require("readline");
+const readline = require("readline");
 
-const ask = function() {
-    return new Promise((resolve, reject) => {
-        rl = readline.createInterface({
-            input: process.stdin,
-            output: process.stdout
-        })
-        rl.question("Enter a list of number(space seperated): ", function(answer){
-            resolve(answer);
-        })
-    })
-}
+const ask = async function() {
+  return new Promise((resolve, reject) => {
+    const rl = readline.createInterface({
+      input: process.stdin,
+      output: process.stdout
+    });
+    rl.question("Enter a list of number(space seperated): ", function(answer) {
+      rl.close();
+      resolve(answer);
+    });
+  });
+};
 
-export default ask;
+module.exports = ask;
--- index.js
+++ index.js
@@ -1,39 +1,88 @@
-const ask = require('./ask.js');
-
- //列與行的pointer
+const ask = require("./ask.js");
+const readline = require("readline");
 
 function gauss_solution() {
-    let userInput;
-    let augMatrix;
-    let rowPointer = 0, colPointer = 0;
-    let tempBuffer; //存垃圾
-    for (let i = 0;userInput !== 'x'; i++) {
-        userInput = await ask();
-        let userInputVector = userInput.split(' ');
-        augMatrix[i] = userInputVector;
+  getAugMatrix().then(augMatrix => {
+    for (
+      let rowPointer = 0, colPointer = 0;
+      rowPointer < augMatrix.length;
+      rowPointer++, colPointer++
+    ) {
+      for (
+        let i = rowPointer + 1;
+        augMatrix[rowPointer][colPointer] === 0 && i < augMatrix.length;
+        i++
+      ) {
+        if (augMatrix[i][colPointer] !== 0) {
+          augMatrix = rowInterchange(augMatrix, rowPointer, i);
+          break;
+        } //將列調換使augMatrix[rowPointer][colPointer]為引導係數
+      }
+      augMatrix = simplifyMatrix(augMatrix, rowPointer, colPointer);
     }
-    for(let i = rowPointer; (augMatrix[rowPointer][colPointer] === 0) && i < augMatrix.length; i++) {
-        augMatrix = rowExchange(augMatrix, rowPointer, (rowPointer+1)/augMatrix.length);
-    }
+    printMatrix(augMatrix);
+  });
+}
 
+async function getAugMatrix() {
+  let userInput;
+  let augMatrix = [];
+  for (let i = 0; userInput !== "x"; i++) {
+    userInput = await ask();
+    if (userInput !== "x") {
+      let userInputArray = userInput.split(" ");
+      augMatrix.push(
+        userInputArray.map(val => {
+          return parseInt(val);
+        })
+      );
+    }
+  }
+  return augMatrix;
 }
 
 function rowInterchange(matrix, row1, row2) {
-    const temp = matrix(row1);
-    matrix[row1] = matrix[row2];
-    matrix[row2] = temp; 
-    return matrix;
+  const temp = matrix(row1);
+  matrix[row1] = matrix[row2];
+  matrix[row2] = temp;
+  return matrix;
 }
 
-function letRowFixed(matrix, rowPointer, colPointer) {
-    for(let i = rowPointer; i% matrix.length !== rowPointer; i++) {
-
-    }
+function simplifyMatrix(matrix, rowPointer, colPointer) {
+  //讓陣列的某一列出現 引導係數 1
+  matrix[rowPointer] = matrix[rowPointer].map(val => {
+    return val / matrix[rowPointer][colPointer];
+  });
+  for (let i = rowPointer + 1; i % matrix.length !== rowPointer; i++) {
+    const targetIndex = i % matrix.length;
+    const subtrahendArray = arrayMultiplicate(
+      matrix[rowPointer],
+      matrix[targetIndex][colPointer]
+    );
+    matrix[targetIndex] = arraySubtract(matrix[targetIndex], subtrahendArray); //ex. R(1) = R(1) - R(0)
+  }
+  return matrix;
 }
 
-function arrayMultiplicate(array1, mul) {
-
+function printMatrix(matrix) {
+  console.log(
+    matrix
+      .map(val => {
+        return val.join(" ");
+      })
+      .join("\n")
+  );
 }
 
+function arrayMultiplicate(arr, mul) {
+  return arr.map(val => val * mul);
+}
+
+function arraySubtract(arr1, arr2) {
+  //arr1 - arr2
+  return arr1.map((val, index) => {
+    return val - arr2[index];
+  });
+}
 
 gauss_solution();
